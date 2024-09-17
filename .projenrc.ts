7f532ddc3f5fd6a291bfa0d1c09046c65934a6e0
Jan Brauer <jan.brauer@superluminar.io>
2024-09-17 17:10:14 +0200

    chore: Update project configuration and metadata

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,12 +1,20 @@
 import { typescript } from 'projen';
-const project = new typescript.TypeScriptAppProject({
+import { NpmAccess } from 'projen/lib/javascript';
+
+const project = new typescript.TypeScriptProject({
   defaultReleaseBranch: 'main',
-  name: 'config-kitty',
+  name: '@superluminar-io/config-kitty',
   projenrcTs: true,
   autoDetectBin: true,
   license: 'MIT',
+  authorOrganization: true,
+  authorUrl: 'https://superluminar.io',
   authorName: 'superluminar GmbH',
   deps: ['@inquirer/prompts'], /* Runtime dependencies of this module. */
   description: 'ConfigKitty: Purrfect eslint/prettier setup for your CDK codebase',
+  releaseToNpm: true,
+  release: true,
+  package: true,
+  npmAccess: NpmAccess.PUBLIC,
 });
 project.synth();
